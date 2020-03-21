b775a3028d3d9aeeea7391d9d0579bdc7286e73b
Brad Ahrens <bradley.ahrens@kwanko.com>
2020-03-21 18:18:03 +0000

    chore: add currency service

--- app/Product.php
+++ app/Product.php
@@ -3,8 +3,14 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use App\Services\CurrencyService;
 
 class Product extends Model
 {
     protected $fillable = ['name', 'price'];
+
+    public function getPriceEurAttribute()
+    {
+        return (new CurrencyService())->convert($this->price, 'usd', 'eur');
+    }
 }
---
+++ tests/Feature/ProductsTest.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Product;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class ProductsTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function test_homepage_contains_empty_products_table()
+    {
+        $this->withoutExceptionHandling();
+
+        $response = $this->get('/');
+
+        $response->assertStatus(200);
+
+        $response->assertSee('No products found');
+    }
+
+    public function test_homepage_contains_non_empty_products_table()
+    {
+        $product = Product::create([
+            'name' => 'Product1',
+            'price' => 99.99
+        ]);
+
+        $this->withoutExceptionHandling();
+
+        $response = $this->get('/');
+
+        $response->assertStatus(200);
+
+        $response->assertDontSee('No products found.');
+
+        $response->assertSee($product->name);
+
+        $view_products = $response->viewData('products');
+
+        $this->assertEquals($product->name, $view_products->first()->name);
+    }
+}
